b150816def71ce9baeef3e2152b455ba9f1cfeff
Juntae <junepark95@hotmail.com>
2021-08-31 18:09:24 -0400

    Finished show and index methods. Added index view for showing all ip_address that hit the token url.

--- app/controllers/token_urls_controller.rb
+++ app/controllers/token_urls_controller.rb
@@ -1,4 +1,6 @@
 class TokenUrlsController < ApplicationController
+  before_action :set_token_url, only: [:show, :index]
+
   def new
     @token_url = TokenUrl.new
   end
@@ -11,12 +13,27 @@ class TokenUrlsController < ApplicationController
   end
 
   def show
+    if @token_url
+      @token_url.internet_protocols.create(address: request.remote_ip)
+      redirect_to @token_url.url
+    else
+      redirect_to root_url, flash: { error: "Token do not exist" } 
+    end
   end
   
   def index
+    if @token_url
+      @ip_addresses = @token_url.internet_protocols
+    else
+      redirect_to root_url, flash: { error: "Token do not exist" } 
+    end
   end
 
   private
+
+  def set_token_url
+    @token_url = TokenUrl.find_by(token: params[:token])
+  end
 
   def token_url_params
     params[:token_url].permit(
